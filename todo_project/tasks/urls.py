e7746f8b57c71b68f54cd461c2f6015f0f083c1b
Patrick <juanpatrick.gallardo@gmail.com>
2025-01-08 19:34:04 +0800

    Created api routes for api functions

---
+++ todo_project/tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import TaskCreateView, TaskListView, TaskDetailView, TaskUpdateView, TaskDeleteView
+
+urlPatterns = [
+    path('tasks/', TaskListView.as_view(), name='task-list'),
+    path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
+    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+]
